6d62a5969a895a2d41c42bc8ca0025c46959fed0
unknown <sjy980330@naver.com>
2021-07-14 23:36:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+
+
+}
