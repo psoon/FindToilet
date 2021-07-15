e2e1adca8e16f88dcff7cb34544f81d10f2f5bbd
psoon <kg111903@naver.com>
2021-07-15 14:34:32 +0900

    map success

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
-
-import android.view.ViewGroup;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.RelativeLayout;
 
 import net.daum.mf.map.api.MapView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MapView mapView = new MapView(this);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        MapView mapView = new MapView(this);
+        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
     }
-
 
 }
