3c02bb723ab18c1340f29cc0ec605b695ae1a1f6
psoon <kg111903@naver.com>
2021-07-23 00:50:38 +0900

    import retrofit2

---
+++ app/src/main/java/com/example/mainactivity/RetrofitApiClient.java
@@ -0,0 +1,18 @@
+package com.example.mainactivity;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitApiClient {
+    private static final String BASE_URL = "https://dapi.kakao.com/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getApiClient(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
