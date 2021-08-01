aadd3468dc573a801eca3a3214b3daf1bdf3fa61
yeaeun21 <yeaeun28@naver.com>
2021-08-01 22:39:49 +0900

    필터xml

---
+++ app/src/main/java/com/example/mainactivity/Filter.java
@@ -0,0 +1,15 @@
+package com.example.mainactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mainactivity.R;
+
+public class Filter extends AppCompatActivity {
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.filter);
+    }
+}
